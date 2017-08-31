3310701539b59532d3ce67f9957619033d887c40
github username <github email address>
2017-08-31 16:15:17 -0400

    Done.

--- src/components/Pikachu.js
+++ src/components/Pikachu.js
@@ -10,6 +10,9 @@ class Pikachu extends React.Component {
     };
   }
 
+  componentDidUpdate(prevProps, prevState){
+    this.resizePikachu();
+  }
   resizePikachu = () => {
     const pikachu = document.getElementById("pikachu");
     pikachu.height = this.state.size;
